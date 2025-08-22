5064e7bb9c82f02c74d844a0023ca6f115aae9df
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-08-22 15:16:48 -0500

    retrieve single userinfo by id

--- tressreliefapi/views/user_info.py
+++ tressreliefapi/views/user_info.py
@@ -10,9 +10,10 @@ from tressreliefapi.serializers import UserInfoSerializer
 class UserInfoView(ViewSet):
     """Tress Relief UserInfo view"""
 
+    # /userinfo, /userinfo?role=:role
     def list(self, request):
         """Handle GET requests for UserInfo
-        You can filter with ?role=stylist, ?role=admin, or ?role=client
+        You can list all and filter with ?role=stylist, ?role=admin, or ?role=client
         """
 
         # get all the userinfos
@@ -24,4 +25,14 @@ class UserInfoView(ViewSet):
             userinfos = userinfos.filter(role=role)
 
         serializer = UserInfoSerializer(userinfos, many=True)
+        return Response(serializer.data)
+
+    # /userinfo/:id
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for a single userinfo by id"""
+        try:
+            userinfo = UserInfo.objects.get(pk=pk)
+        except UserInfo.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        serializer = UserInfoSerializer(userinfo)
         return Response(serializer.data)
